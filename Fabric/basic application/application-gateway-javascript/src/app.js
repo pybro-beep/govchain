f692b37362b4cee15a41f1afc5f76af5317456a6
pybro-beep <153233276+pybro-beep@users.noreply.github.com>
2024-12-06 09:05:56 +0100

    cleanup

--- Fabric/basic application/application-gateway-javascript/src/app.js
+++ Fabric/basic application/application-gateway-javascript/src/app.js
@@ -184,16 +184,17 @@ async function newSigner() {
 async function handleTTL(contract, payload, txid) {
     // used for purging private data from all peers if it has not happened automatically because of blocksToLive of the collection
     if (!payload.ttl) {
-        console.log(`ERROR (handleTTL): was called on payload which does not have a TTL property (${typeof(payload)}): ${payload}`);
+        console.log(`STATE (handleTTL): was called on payload which does not have a TTL property (${typeof(payload)}): ${payload}`);
         return;
     }
+    // calculate if TTL has ended
     const creationTime = new Date(Date.parse(payload.timestamp));
     const currentTime = new Date();
     const timeToLive = new Date().setDate(creationTime.getDate() + payload.ttl);
+    // if TTL has ended, purge private data
     if (currentTime.getDate() > timeToLive.getDate()) {
-        // TODO: purge private data
         contract.submitTransaction('Purge', JSON.stringify(txid));
-        console.log(`SUCCESS (handleTTL): purged private data of ${txid}`);
+        console.log(`SUCCESS (handleTTL): purged private data of ${txid} because TTL is ${(currentTime - timeToLive).toString()}`);
     }
 }
 async function handleResponse(contract, payload, txid) {
@@ -235,11 +236,7 @@ async function handleRequest(contract, payload, txid) {
     const update = await setStatus(contract, txid, result_txid);
     console.log(`\tSUCCESS (handleRequest): updated status ${update}`);
 }
-async function initLedger(contract) {
-    await contract.submitTransaction('InitLedger');
-    console.log('SUCCESS (initLedger): initialized Ledger');
-}
-async function setStatus(contract, requestTxid, responseTxid) { //TODO: rewrite (use updateAsset instead, this is weird!)
+async function setStatus(contract, requestTxid, responseTxid) { 
     const new_public = await getPublic();
     new_public.status = responseTxid;
     await updatePublic(contract, requestTxid, new_public)
@@ -304,147 +301,6 @@ async function createAsset(contract, pub, priv) { //returns txid!
         console.error(err)
     }
 }
-async function getExisting(contract) {
-    console.log(
-        '\n--> Evaluate Transaction: GetAllAssets, function returns all the current assets on the ledger'
-    );
-
-    const resultBytes = await contract.evaluateTransaction('GetAllNotFrom', peerHostAlias);
-
-    const resultJson = utf8Decoder.decode(resultBytes);
-    const requests = JSON.parse(resultJson);
-    console.log('*** Existing Requests:', requests);
-    return requests;
-}
-// same as createAsset, but adds asset to private collection instead
-async function createPrivateAsset(contract) {
-    console.log('\n--> Submit Transaction: CreatePrivateAsset, creating a private asset');
-
-    const assetTransient = {
-        asset: Buffer.from(
-            JSON.stringify({
-                ID: 'privateAsset1',
-                Color: 'blue',
-                Size: 10,
-                Owner: 'Alice',
-                AppraisedValue: 500
-            })
-        ),
-    };
-
-    // Create the transaction and set transient data
-    const transaction = contract.createTransaction('CreatePrivateAsset');
-    transaction.setTransient(assetTransient);
-
-    // Submit the transaction
-    await transaction.submit();
-    console.log('*** Private Asset created successfully');
-}
-
-/*
-same as createAsset, but adds asset to the shared private data collection between org1 and org2.
-This function will propagate the private data (transient data) from the creating peer to the peers of Org1 and Org2
-using the Gossip protocol.
-*/
-//WARN: will error if client peer does not fullfill the policy requirements for the sharedPrivateCollection
-async function createSharedPrivateAsset(contract) {
-    console.log('\n--> Submit Transaction: CreateSharedPrivateAsset, creating a shared private asset');
-
-    const assetTransient = {
-        asset: Buffer.from(
-            JSON.stringify({
-                ID: 'sharedAsset1',
-                Color: 'red',
-                Size: 15,
-                Owner: 'Alice',
-                AppraisedValue: 1000
-            })
-        ),
-    };
-
-    // submit the transaction with transient data
-    try {
-        const transaction = await contract.submit('CreateSharedPrivateAsset', {transientData: assetTransient,});
-        console.log('***** Transaction CreateSharedPrivateAsset Success');
-        console.log(transaction.toString());
-    } catch (err) {
-        console.log('***** Transaction CreateSharedPrivateAsset Failed due to Error:');
-        console.error(err);
-    }
-}
-
-
-
-
-/**
- * Submit transaction asynchronously, allowing the application to process the smart contract response (e.g. update a UI)
- * while waiting for the commit notification.
- */
-async function transferAssetAsync(contract) {
-    console.log(
-        '\n--> Async Submit Transaction: TransferAsset, updates existing asset owner'
-    );
-
-    const commit = await contract.submitAsync('TransferAsset', {
-        arguments: [assetId, 'Saptha'],
-    });
-    const oldOwner = utf8Decoder.decode(commit.getResult());
-
-    console.log(
-        `*** Successfully submitted transaction to transfer ownership from ${oldOwner} to Saptha`
-    );
-    console.log('*** Waiting for transaction commit');
-
-    const status = await commit.getStatus();
-    if (!status.successful) {
-        throw new Error(
-            `Transaction ${
-                status.transactionId
-            } failed to commit with status code ${String(status.code)}`
-        );
-    }
-
-    console.log('*** Transaction committed successfully');
-}
-
-async function readAssetByID(contract) {
-    console.log(
-        '\n--> Evaluate Transaction: ReadAsset, function returns asset attributes'
-    );
-
-    const resultBytes = await contract.evaluateTransaction(
-        'ReadAsset',
-        assetId
-    );
-
-    const resultJson = utf8Decoder.decode(resultBytes);
-    const result = JSON.parse(resultJson);
-    console.log('*** Result:', result);
-}
-
-/**
- * submitTransaction() will throw an error containing details of any error responses from the smart contract.
- */
-async function updateNonExistentAsset(contract) {
-    console.log(
-        '\n--> Submit Transaction: UpdateAsset asset70, asset70 does not exist and should return an error'
-    );
-
-    try {
-        await contract.submitTransaction(
-            'UpdateAsset',
-            'asset70',
-            'blue',
-            '5',
-            'Tomoko',
-            '300'
-        );
-        console.log('******** FAILED to return an error');
-    } catch (error) {
-        console.log('*** Successfully caught the error: \n', error);
-    }
-}
-
 /**
  * envOrDefault() will return the value of an environment variable, or a default value if the variable is undefined.
  */
