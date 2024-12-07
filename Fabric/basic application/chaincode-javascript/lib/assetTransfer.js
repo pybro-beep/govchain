d41dd4be6ca53a5a41849ccc6c038b130a8047e7
pybro-beep <153233276+pybro-beep@users.noreply.github.com>
2024-12-07 16:15:15 +0100

    cleanup

--- Fabric/basic application/chaincode-javascript/lib/assetTransfer.js
+++ Fabric/basic application/chaincode-javascript/lib/assetTransfer.js
@@ -51,6 +51,7 @@ class AssetTransfer extends Contract {
         }
         // if it does, delete it
         ctx.stub.deletePrivateData(privateCollectionName, txid);
+        // remove TTL, because te(txid, JSON.stringify(data));
         return txid;
     }
     async GetPublic(ctx, txid) {
@@ -115,95 +116,6 @@ class AssetTransfer extends Contract {
         await ctx.stub.putPrivateData(privateCollectionName, key, Buffer.from(stringify(sortKeysRecursive(priv))));
         return key.toString();
     }
-    // create private asset by writing transient data
-    async CreatePrivateAsset(ctx, id, color, size, owner, appraisedValue) {
-        const transientData = ctx.stub.getTransient();
-        if (!transientData.has('asset')) {
-            throw new Error('The transient data must contain an "asset" key');
-        }
-
-        const assetData = transientData.get('asset').toString('utf8');
-        const asset = JSON.parse(assetData);
-
-        if (!asset.ID || !asset.Color || !asset.Size || !asset.Owner || !asset.AppraisedValue) {
-            throw new Error('Asset object must contain ID, Color, Size, Owner, and AppraisedValue');
-        }
-
-        const privateCollectionName = `Org${ctx.clientIdentity.getMSPID()}MSPPrivateCollection`;
-
-        await ctx.stub.putPrivateData(privateCollectionName, asset.ID, Buffer.from(JSON.stringify(asset)));
-        return JSON.stringify(asset);
-    }
-    // create private data shared between the two orgs using their shared private data collection
-    async CreateSharedPrivateAsset(ctx) {
-        const transientData = ctx.stub.getTransient();
-        if (!transientData.has('asset')) {
-            throw new Error('The transient data must contain an "asset" key');
-        }
-
-        const assetData = transientData.get('asset').toString('utf8');
-        const asset = JSON.parse(assetData);
-
-        if (!asset.ID || !asset.Color || !asset.Size || !asset.Owner || !asset.AppraisedValue) {
-            throw new Error('Asset object must contain ID, Color, Size, Owner, and AppraisedValue');
-        }
-
-        const sharedCollectionName = 'SharedPrivateCollection';
-
-        await ctx.stub.putPrivateData(sharedCollectionName, asset.ID, Buffer.from(JSON.stringify(asset)));
-        return JSON.stringify(asset);
-    }
-
-
-
-    // ReadAsset returns the asset stored in the world state with given id.
-    async ReadAsset(ctx, id) {
-        const assetJSON = await ctx.stub.getState(id); // get the asset from chaincode state
-        if (!assetJSON || assetJSON.length === 0) {
-            throw new Error(`The asset ${id} does not exist`);
-        }
-        return assetJSON.toString();
-    }
-    // add ReadAsset support for the use of private collections of the current client
-    async ReadPrivateAsset(ctx, id) {
-        const privateCollectionName = `Org${ctx.clientIdentity.getMSPID()}MSPPrivateCollection`;
-        const assetJSON = await ctx.stub.getPrivateData(privateCollectionName, id);
-        if (!assetJSON || assetJSON.length === 0) {
-            throw new Error(`Private asset with ID ${id} does not exist`);
-        }
-        return assetJSON.toString();
-    }
-    // ReadAsset support for reading data from the shared private data collection between org1 and org2
-    async ReadSharedPrivateAsset(ctx, id) {
-        const sharedCollectionName = 'SharedPrivateCollection';
-        const assetJSON = await ctx.stub.getPrivateData(sharedCollectionName, id);
-        if (!assetJSON || assetJSON.length === 0) {
-            throw new Error(`Shared private asset with ID ${id} does not exist`);
-        }
-        return assetJSON.toString();
-    }
-
-
-
-    // UpdateAsset updates an existing asset in the world state with provided parameters.
-    async UpdateAsset(ctx, id, color, size, owner, appraisedValue) {
-        const exists = await this.AssetExists(ctx, id);
-        if (!exists) {
-            throw new Error(`The asset ${id} does not exist`);
-        }
-
-        // overwriting original asset with new asset
-        const updatedAsset = {
-            ID: id,
-            Color: color,
-            Size: size,
-            Owner: owner,
-            AppraisedValue: appraisedValue,
-        };
-        // we insert data in alphabetic order using 'json-stringify-deterministic' and 'sort-keys-recursive'
-        return ctx.stub.putState(id, Buffer.from(stringify(sortKeysRecursive(updatedAsset))));
-    }
-
     // DeleteAsset deletes an given asset from the world state.
     async DeleteAsset(ctx, id) {
         const exists = await this.AssetExists(ctx, id);
@@ -212,52 +124,10 @@ class AssetTransfer extends Contract {
         }
         return ctx.stub.deleteState(id);
     }
-    // DeleteAsset for private data collections
-    async DeletePrivateAsset(ctx, id) {
-        const privateCollectionName = `Org${ctx.clientIdentity.getMSPID()}MSPPrivateCollection`;
-        await ctx.stub.deletePrivateData(privateCollectionName, id);
-    }
-
-
     // AssetExists returns true when asset with given ID exists in world state.
     async AssetExists(ctx, id) {
         const assetJSON = await ctx.stub.getState(id);
         return assetJSON && assetJSON.length > 0;
-    }
-
-    // TransferAsset updates the owner field of asset with given id in the world state.
-    async TransferAsset(ctx, id, newOwner) {
-        const assetString = await this.ReadAsset(ctx, id);
-        const asset = JSON.parse(assetString);
-        const oldOwner = asset.Owner;
-        asset.Owner = newOwner;
-        // we insert data in alphabetic order using 'json-stringify-deterministic' and 'sort-keys-recursive'
-        await ctx.stub.putState(id, Buffer.from(stringify(sortKeysRecursive(asset))));
-        return oldOwner;
-    }
-    // TODO: get all requests until current ID or no Answer
-    async GetNextRequest(ctx, id, peer) {
-        // FIXME: specify range parameters according to Documentation -> adjustable for better performance
-        // TODO: if id is empty, get oldest
-        const iterator = await ctx.stub.getStateByRange('', '');
-        let result = await iterator.next();
-        const answers = [];
-        while (!result.done) {
-            strValue = Buffer.from(result.value.value.toString()).toString('utf-8');
-            let record;
-            try {
-                record = JSON.parse(strValue);
-                if (record.request_id) {//record is a response
-                    answers.push(request_id);
-                } else if (!record.requester === peer && !answers.includes(record.id) && record.id > id) {
-                    return JSON.stringify(record)
-                }
-            } catch (err) {
-                console.error(`Failed to parse: ${strValue}`);
-                record = strValue;
-            }
-        }
-        return '';
     }
     // GetAllAssets returns all assets found in the world state.
     async GetAllAssets(ctx) {
